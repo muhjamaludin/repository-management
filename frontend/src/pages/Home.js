39c2d219b19b155d3e330fae626967ecac3d61ef
Muh Jamaludin <74maludin@gmail.com>
2023-01-24 09:00:37 +0700

    integrate fetch data from backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Table } from "reactstrap";
+import axios from "axios";
 import Header from "../components/Header";
 
 function Home() {
+  const [repos, setRepos] = useState([]);
+
+  const fetchData = () => {
+    return axios
+      .get("http://localhost:9010/api/repositories")
+      .then((resp) => setRepos(resp.data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <Header />
-      <h1>Table</h1>
+      {console.log(repos)}
+      <Table>
+        <thead>
+          <tr>
+            {Object.keys(repos.data).length > 0 &&
+              Object.keys(repos.data[0]).map((repo, i) => {
+                return <th key={i}>{repo}</th>;
+              })}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </>
   );
 }
